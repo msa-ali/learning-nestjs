e914550f321ac71688c6851f0d5d96f68a082b59
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2021-12-21 07:47:39 +0530

    user service implementation

---
+++ used-car-pricing-api/src/users/dtos/update-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, isEmail, IsOptional, IsString, isString } from "class-validator";
+
+export class UpdateUserDto {
+
+    @IsEmail()
+    @IsOptional()
+    email: string;
+
+    @IsString()
+    @IsOptional()
+    password: string;
+}
--- used-car-pricing-api/src/users/users.service.ts
+++ used-car-pricing-api/src/users/users.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(@InjectRepository(User) private repo: Repository<User>) { }
+
+    create(email: string, password: string) {
+        const user = this.repo.create({ email, password }); // Hooks are executed
+
+        return this.repo.save(user); // Hooks are not executed
+    }
+
+    findOne(id: number) {
+        return this.repo.findOne(id);
+    }
+
+    find(email: string) {
+        return this.repo.find({ email })
+    }
+
+    async update(id: number, attrs: Partial<User>) {
+        const user = await this.findOne(id);
+
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+        Object.assign(user, attrs);
+        return this.repo.save(user);
+    }
+
+    async remove(id: number) {
+        const user = await this.findOne(id);
+
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+
+        return this.repo.remove(user);
+
+    }
+}
